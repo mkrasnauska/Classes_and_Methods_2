d70fc299f9922782467f69067323e68166f242c5
Margarita <margaritakrasnauska@gmail.com>
2023-02-14 13:17:37 +0000

    Add project files.

---
+++ Classes_and_Methods_2/Math.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMethodsAssignment2
+{
+    public class Math
+    {
+        public static int MySumInt(int a, int b = 1)
+        {
+            return a + b;
+        }
+
+        public static int MyTimesInt(int a, int b = 1)
+        {
+            return a * b;
+        }
+    }
+}
---
+++ Classes_and_Methods_2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMethodsAssignment2
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int x = Math.MySumInt(399);
+            Console.WriteLine("399 +optional value = " + x);
+            Console.WriteLine("Please type in a first number ");
+            int m = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please type in second Number (optional)");
+
+            string n = Console.ReadLine();
+            if (n == "")
+            {
+                int y = Math.MyTimesInt(m);
+                Console.WriteLine("result: " + y);
+                Console.ReadLine();
+
+
+            }
+            else
+            {
+
+                int y = Math.MyTimesInt(m, Convert.ToInt32(n));
+                Console.WriteLine("result: " + y);
+                Console.ReadLine();
+            }
+        }
+    }
+}
